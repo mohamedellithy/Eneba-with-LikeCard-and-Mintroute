a497b2a6333df1bd565891273d6985a36c6f9cb1
mohamedellithy <develomohamed22@gmail.com>
2023-07-06 19:19:36 +0300

    layout dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/master', function () {
+    return view('pages.dashboard');
+});
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
