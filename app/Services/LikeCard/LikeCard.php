e06f159213e09c89654f47d68fe82d25c2577140
mohamedellithy <develomohamed22@gmail.com>
2023-07-13 17:31:02 +0300

    sswswwwwwصqwg

--- app/Services/LikeCard/LikeCard.php
+++ app/Services/LikeCard/LikeCard.php
@@ -68,7 +68,7 @@ class LikeCard {
             'password'     => isset($this->credentail['prod_password']) ? $this->credentail['prod_password'] : null,
             'securityCode' => isset($this->credentail['prod_securityCode']) ? $this->credentail['prod_securityCode'] : null,
             'langId'       => '1',
-            'ids[]'        => '',
+            'categoryId'   => '59',
             'page'         => $page
         ];
 
