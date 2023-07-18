411d047069b4c81f25001f2f08b3ababf6559d35
mohamedellithy <develomohamed22@gmail.com>
2023-07-18 13:32:42 +0300

    test likecard #4

--- app/Http/Controllers/LikeCardApplication.php
+++ app/Http/Controllers/LikeCardApplication.php
@@ -62,7 +62,7 @@ class LikeCardApplication extends Controller
     }
 
     public function get_products(){
-        $products = $this->likecard_service->get_products();
+        $products = collect($this->likecard_service->get_products());
         dd($products);
         return view('pages.likecard.products.index',compact('products'));
     }
