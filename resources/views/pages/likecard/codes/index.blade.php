2741ebdd8b8919d79ba8f5f59a128e5f29301d8f
mohamedellithy <develomohamed22@gmail.com>
2023-08-02 16:05:12 +0300

    sqwqwqsq

--- resources/views/pages/likecard/codes/index.blade.php
+++ resources/views/pages/likecard/codes/index.blade.php
@@ -9,39 +9,37 @@ $category_id = request()->query('category_id') ?: null;
         </h4>
         <!-- Basic Bootstrap Table -->
         <div class="card" style="padding-top: 3%;">
-            <form id="filter-data" method="get">
-                <div class="d-flex filters-fields">
-                    <div class="nav-item d-flex align-items-center m-2" >
-                        <form id="filter-data" method="get">
-                            <select name="category_id" onchange="document.getElementById('filter-data').submit()" id="largeSelect" class="form-select form-select-lg">
-                                <option>تصنيفات المنتجات</option>
-                                @isset($categories)
-                                    @foreach($categories as $category)
-                                        <option value="{{ $category['id'] }}" @isset($category_id) @if($category_id == $category['id']) selected @endif @endisset>{{ $category['categoryName'] }}</option>
-                                        @isset($category['childs'])
-                                            @foreach($category['childs'] as $child)
-                                                <option value="{{ $child['id'] }}" @isset($category_id) @if($category_id == $child['id']) selected @endif @endisset> -- {{ $child['categoryName'] }}</option>
-                                            @endforeach
-                                        @endisset
-                                    @endforeach
-                                @endisset
-                            </select>
-                        </form>
-                    </div>
-                    <div class="nav-item d-flex align-items-center m-2" >
-                        <select name="product_id" onchange="document.getElementById('filter-data').submit()" id="largeSelect" class="form-select form-select-lg">
-                            <option>المنتجات</option>
-                            @isset($products)
-                                @foreach($products as $product)
-                                    <option value="{{ $product['productId'] }}">{{ $product['productName'] }}</option>
+            <div class="d-flex">
+                <div class="nav-item d-flex align-items-center m-2" >
+                    <form id="filter-data" method="get">
+                        <select name="category_id" onchange="document.getElementById('filter-data').submit()" id="largeSelect" class="form-select form-select-lg">
+                            <option>تصنيفات المنتجات</option>
+                            @isset($categories)
+                                @foreach($categories as $category)
+                                    <option value="{{ $category['id'] }}" @isset($category_id) @if($category_id == $category['id']) selected @endif @endisset>{{ $category['categoryName'] }}</option>
+                                    @isset($category['childs'])
+                                        @foreach($category['childs'] as $child)
+                                            <option value="{{ $child['id'] }}" @isset($category_id) @if($category_id == $child['id']) selected @endif @endisset> -- {{ $child['categoryName'] }}</option>
+                                        @endforeach
+                                    @endisset
                                 @endforeach
                             @endisset
                         </select>
-                    </div>
-
+                    </form>
                 </div>
-            </form>
-            <br/>
+                <div class="nav-item d-flex align-items-center m-2" >
+                    <select name="product_id" onchange="document.getElementById('filter-data').submit()" id="largeSelect" class="form-select form-select-lg">
+                        <option>المنتجات</option>
+                        @isset($products)
+                            @foreach($products as $product)
+                                <option value="{{ $product['productId'] }}">{{ $product['productName'] }}</option>
+                            @endforeach
+                        @endisset
+                    </select>
+                </div>
+            </div>
+        </div>
+        <div class="card" style="padding-top: 3%;">
             <div class="table-responsive">
                 <table class="table table-striped">
                     <thead>
