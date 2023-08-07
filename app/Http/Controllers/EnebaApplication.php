c92216f25598e6724ebbf76ee526b5d0bab11123
mohamedellithy <develomohamed22@gmail.com>
2023-08-07 20:24:42 +0300

    djdwssswswswssswwصewdwwewwswse

--- app/Http/Controllers/EnebaApplication.php
+++ app/Http/Controllers/EnebaApplication.php
@@ -68,10 +68,9 @@ class EnebaApplication extends Controller
     public function eneba_callback(Request $request){
         Http::post('https://webhook.site/4e302f27-0ad9-43ea-8573-0969708fb17e',$request->all());
         return response()->json([
-          'status'   => 'RESERVE',
-          "action"   => "RESERVE",
-          'RESERVE'  => true,
-          'received' => true
+            "action"  => "RESERVE",
+            "orderId" => $request->input('orderId'),
+            "success" => true
         ],200);
     }
 
