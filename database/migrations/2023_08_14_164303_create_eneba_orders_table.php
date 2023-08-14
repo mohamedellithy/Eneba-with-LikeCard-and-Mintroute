39991d166e3c0c3f2db81f95b36a67e851214bfa
mohamedellithy <develomohamed22@gmail.com>
2023-08-14 20:00:21 +0300

    djd

---
+++ database/migrations/2023_08_14_164303_create_eneba_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eneba_orders', function (Blueprint $table) {
+            $table->id();
+            $table->text('order_id');
+            $table->string('status_order');
+            $table->text('auctions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eneba_orders');
+    }
+};
