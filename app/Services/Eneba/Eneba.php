ee649c58a2d12a1755094f8d5850069a7a9c409e
mohamedellithy <develomohamed22@gmail.com>
2023-08-14 12:28:08 +0300

    djdjwwwwwwwwwwwdwwwswsss

--- app/Services/Eneba/Eneba.php
+++ app/Services/Eneba/Eneba.php
@@ -234,30 +234,10 @@ class Eneba {
         // GQL;
 
 
-        $query = <<<GQL
-            mutation {
-                P_triggerCallback(input: {
-                type: DECLARED_STOCK_RESERVATION
-                orderId: "347c4978-4f81-11ed-bdc3-0242ac120002"
-                auction: {
-                    auctionId: "347c4e96-4f81-11ed-bdc3-0242ac120002"
-                    price: {
-                    amount: 1500
-                    currency: "EUR"
-                    }
-                    keyCount: 1
-                }
-                }){
-                success
-                message
-                }
-            }
-        GQL;
-
         // $query = <<<GQL
         //     mutation {
         //         P_triggerCallback(input: {
-        //         type: DECLARED_STOCK_PROVISION
+        //         type: DECLARED_STOCK_RESERVATION
         //         orderId: "347c4978-4f81-11ed-bdc3-0242ac120002"
         //         auction: {
         //             auctionId: "347c4e96-4f81-11ed-bdc3-0242ac120002"
@@ -273,6 +253,26 @@ class Eneba {
         //         }
         //     }
         // GQL;
+
+        $query = <<<GQL
+            mutation {
+                P_triggerCallback(input: {
+                type: DECLARED_STOCK_PROVISION
+                orderId: "347c4978-4f81-11ed-bdc3-0242ac120002"
+                auction: {
+                    auctionId: "347c4e96-4f81-11ed-bdc3-0242ac120002"
+                    price: {
+                    amount: 1500
+                    currency: "EUR"
+                    }
+                    keyCount: 1
+                }
+                }){
+                success
+                message
+                }
+            }
+        GQL;
 
         // $response = $this->resolve_call($query);
         // dd($response->json());
