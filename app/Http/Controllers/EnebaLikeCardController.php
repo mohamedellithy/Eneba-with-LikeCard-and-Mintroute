d5fabe694a7f8f1f2098db561530f6618c95cd75
mohamedellithy <develomohamed22@gmail.com>
2023-08-15 17:26:17 +0300

    sks

--- app/Http/Controllers/EnebaLikeCardController.php
+++ app/Http/Controllers/EnebaLikeCardController.php
@@ -31,6 +31,8 @@ class EnebaLikeCardController extends Controller
 
         $eneba_likecard_product = Product::where('eneba_prod_id',$id)->select('likecard_prod_id');
 
+        dd($eneba_likecard_product);
+
         $category      = null;
         if(request('category_id')):
             $category  = $request->query('category_id');
