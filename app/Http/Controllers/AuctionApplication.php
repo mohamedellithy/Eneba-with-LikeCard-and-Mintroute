4de0f65a954bd936c05d9e908688ed6985476da9
mohamedellithy <develomohamed22@gmail.com>
2023-08-16 16:14:08 +0300

    skwwsssdssssswsswsasssaq

--- app/Http/Controllers/AuctionApplication.php
+++ app/Http/Controllers/AuctionApplication.php
@@ -28,7 +28,7 @@ class AuctionApplication extends Controller
                 return $this->eneba_service->get_products($page_no = null,$search);
             });
         }
-        dd($products);
+        //dd($products);
         return view('pages.auctions.index',compact('auctions','products'));
     }
 
