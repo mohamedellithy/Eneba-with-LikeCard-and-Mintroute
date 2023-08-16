84fa8c68a6c6ce63b75ad1812dbfb0a1537b3a9c
mohamedellithy <develomohamed22@gmail.com>
2023-08-16 15:54:50 +0300

    skwwsssdssssswssw

--- app/Http/Controllers/AuctionApplication.php
+++ app/Http/Controllers/AuctionApplication.php
@@ -4,9 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Auction;
+use App\Services\Eneba\Eneba;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 class AuctionApplication extends Controller
 {
     //
+
+    protected $application;
+    protected $eneba_service;
+
+    public function __construct(){
+        $this->application   = 'eneba';
+        $this->eneba_service = new Eneba($sandbox = false);
+    }
 
     public function index(Request $request){
         $auctions = Auction::paginate(20);
@@ -14,8 +25,12 @@ class AuctionApplication extends Controller
     }
 
     public function ajax_search_on_eneba_products(Request $request){
+        $search    = request('eneba_product_name');
+        $products  = Cache::rememberForever('eneba_products_'.$search, function() use($search){
+            return $this->eneba_service->get_products($page_no = null,$search);
+        });
         return response()->json([
-            'result' => 'hi mohamed'
+            'result' => $products
         ]);
     }
 }
