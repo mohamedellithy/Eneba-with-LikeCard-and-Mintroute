bc178589c7746b087b9d417fdc09c43b9e707f47
mohamedellithy <develomohamed22@gmail.com>
2023-08-18 01:25:56 +0300

    sk1asessdssssssssdsssssswsswس

--- resources/views/pages/auctions/create.blade.php
+++ resources/views/pages/auctions/create.blade.php
@@ -16,28 +16,18 @@ $high_price = $prices->max('amount') / 100;
             <div class="card-body">
                 <form>
                     <div class="mb-3">
-                        <label class="form-label" for="basic-default-fullname">Full Name</label>
-                        <input type="text" class="form-control" id="basic-default-fullname" placeholder="John Doe">
+                        <label class="form-label" for="basic-default-fullname">السعر من لايك كارد</label>
+                        <input type="text" class="form-control" id="basic-default-fullname" readonly>
                     </div>
                     <div class="mb-3">
-                        <label class="form-label" for="basic-default-company">Company</label>
+                        <label class="form-label" for="basic-default-company">الحد الادني للتسعير</label>
                         <input type="text" class="form-control" id="basic-default-company" placeholder="ACME Inc.">
                     </div>
                     <div class="mb-3">
-                        <label class="form-label" for="basic-default-email">Email</label>
+                        <label class="form-label" for="basic-default-email">الحد الأقصي للتسعير</label>
                         <div class="input-group input-group-merge">
                             <input type="text" id="basic-default-email" class="form-control" placeholder="john.doe" aria-label="john.doe" aria-describedby="basic-default-email2">
-                            <span class="input-group-text" id="basic-default-email2">@example.com</span>
                         </div>
-                        <div class="form-text">You can use letters, numbers &amp; periods</div>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label" for="basic-default-phone">Phone No</label>
-                        <input type="text" id="basic-default-phone" class="form-control phone-mask" placeholder="658 799 8941">
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label" for="basic-default-message">Message</label>
-                        <textarea id="basic-default-message" class="form-control" placeholder="Hi, Do you have a moment to talk Joe?"></textarea>
                     </div>
                     <button type="submit" class="btn btn-primary">Send</button>
                 </form>
