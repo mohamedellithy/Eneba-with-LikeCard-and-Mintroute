0b6db9b0f3906d40e641fe1b2fc5e118107cf3ce
mohamedellithy <develomohamed22@gmail.com>
2023-08-17 13:54:02 +0300

    sk1asssw

--- resources/views/pages/auctions/create.blade.php
+++ resources/views/pages/auctions/create.blade.php
@@ -51,44 +51,14 @@
                         <span id="basic-icon-default-fullname2" class="input-group-text"><i
                                 class="bx bx-user"></i></span>
                         <h5>
-                            {{ $product_eneba }}
+                            {{ $product_eneba['S_product']['name'] }}
                         </h5>
                     </div>
                 </div>
-                <div class="mb-3">
-                    <label class="form-label" for="basic-icon-default-company">Company</label>
-                    <div class="input-group input-group-merge">
-                        <span id="basic-icon-default-company2" class="input-group-text"><i
-                                class="bx bx-buildings"></i></span>
-                        <input type="text" id="basic-icon-default-company" class="form-control" placeholder="ACME Inc." aria-label="ACME Inc." aria-describedby="basic-icon-default-company2">
-                    </div>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label" for="basic-icon-default-email">Email</label>
-                    <div class="input-group input-group-merge">
-                        <span class="input-group-text"><i class="bx bx-envelope"></i></span>
-                        <input type="text" id="basic-icon-default-email" class="form-control" placeholder="john.doe" aria-label="john.doe" aria-describedby="basic-icon-default-email2">
-                        <span id="basic-icon-default-email2" class="input-group-text">@example.com</span>
-                    </div>
-                    <div class="form-text">You can use letters, numbers &amp; periods</div>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label" for="basic-icon-default-phone">Phone No</label>
-                    <div class="input-group input-group-merge">
-                        <span id="basic-icon-default-phone2" class="input-group-text"><i
-                                class="bx bx-phone"></i></span>
-                        <input type="text" id="basic-icon-default-phone" class="form-control phone-mask" placeholder="658 799 8941" aria-label="658 799 8941" aria-describedby="basic-icon-default-phone2">
-                    </div>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label" for="basic-icon-default-message">Message</label>
-                    <div class="input-group input-group-merge">
-                        <span id="basic-icon-default-message2" class="input-group-text"><i
-                                class="bx bx-comment"></i></span>
-                        <textarea id="basic-icon-default-message" class="form-control" placeholder="Hi, Do you have a moment to talk Joe?" aria-label="Hi, Do you have a moment to talk Joe?" aria-describedby="basic-icon-default-message2"></textarea>
-                    </div>
-                </div>
-                <button type="submit" class="btn btn-primary">Send</button>
+
+                <a href="{{ 'https://www.eneba.com/'.$product_eneba['S_product']['slug'] }}" class="btn btn-primary">
+                    تفاصييل المنتج
+                </a>
             </div>
         </div>
     </div>
