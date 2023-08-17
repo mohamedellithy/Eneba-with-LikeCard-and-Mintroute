6f2512fc5a250dbb66ec96558c2cf53547cb32ac
mohamedellithy <develomohamed22@gmail.com>
2023-08-18 01:40:49 +0300

    sk1asessdssssssssdsssssswsswسwص

--- resources/views/pages/auctions/create.blade.php
+++ resources/views/pages/auctions/create.blade.php
@@ -10,7 +10,7 @@ $high_price = $prices->max('amount') / 100;
     <div class="col-xl">
         <div class="card mb-4">
             <div class="card-header d-flex justify-content-between align-items-center">
-                <h5 class="mb-0">Basic Layout</h5>
+                <h5 class="mb-0"></h5>
                 <small class="text-muted float-end">Default label</small>
             </div>
             <div class="card-body">
