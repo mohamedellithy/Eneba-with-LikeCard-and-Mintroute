24c2c63696ca65b63fbd000ebf649779cb130e52
mohamedellithy <develomohamed22@gmail.com>
2023-08-23 02:06:00 +0300

    dkwdwwsdswdsddsesswsdss

--- app/Services/Eneba/Operations.php
+++ app/Services/Eneba/Operations.php
@@ -20,11 +20,13 @@ class Operations {
             'status_order' => 'RESERVE',
         ]);
 
-        $auction = Auction::where('auction',request('auctionId'))->first();
-        EnebaOrderAuction::updateOrCreate([
-            'eneba_order_id'  => $eneba_order->id,
-            'eneba_auction_id'=> $auction->id
-        ]);
+        foreach(request('auctions') as $auction):
+            $auction = Auction::where('auction',$auction['auctionId'])->first();
+            EnebaOrderAuction::updateOrCreate([
+                'eneba_order_id'  => $eneba_order->id,
+                'eneba_auction_id'=> $auction->id
+            ]);
+        endforeach;
     }
 
     public static function update_orders_and_get_codes(){
