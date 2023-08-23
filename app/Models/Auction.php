72545ff020c09280489f52034b55a195c216ec33
mohamedellithy <develomohamed22@gmail.com>
2023-08-23 19:22:21 +0300

    dkw1سwdيddded

--- app/Models/Auction.php
+++ app/Models/Auction.php
@@ -12,7 +12,10 @@ class Auction extends Model
     protected $fillable = ['auction','product_id','status','current_price','count_cards','min_price','max_price','automation','change_time','price_step'];
 
     public function orders(){
-        return $this->belongsToMany(EnebaOrder::class,'eneba_order_auctions','eneba_auction_id','eneba_order_id');
+        return $this->belongsToMany(EnebaOrder::class,'eneba_order_auctions','eneba_auction_id','eneba_order_id')->withPivot([
+            'key_count_required',
+            'unit_price'
+        ]);
     }
 
     public function product(){
