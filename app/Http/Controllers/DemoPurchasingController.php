6d2058860ee5e4805f34660ecd3f823a66ca7749
mohamedellithy <develomohamed22@gmail.com>
2023-08-24 15:15:42 +0300

    dks

--- app/Http/Controllers/DemoPurchasingController.php
+++ app/Http/Controllers/DemoPurchasingController.php
@@ -24,11 +24,12 @@ class DemoPurchasingController extends Controller
 
         //$this->eneba_service->eneba_callback_stock_provision();
 
-        //dd($this->eneba_service->sandbox_trigger_stock_reservation());
+        dd($this->eneba_service->sandbox_trigger_stock_reservation());
         // dd($this->eneba_service->sandbox_trigger_stock_provision());
-        $likecard = new LikeCard();
-        $response = $likecard->create_likecard_order(376,5);
-        dd($response);
-        dd($this->eneba_service->eneba_callback_stock_provision());
+       
+        // $likecard = new LikeCard();
+        // $response = $likecard->create_likecard_order(376,5);
+        // dd($response);
+        // dd($this->eneba_service->eneba_callback_stock_provision());
     }
 }
