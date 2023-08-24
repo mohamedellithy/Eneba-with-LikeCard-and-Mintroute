dce0e787f4f68fa41ac13db9b17e68b7a8090ab6
mohamedellithy <develomohamed22@gmail.com>
2023-08-24 15:25:26 +0300

    dksdddsd

--- app/Services/Eneba/Operations.php
+++ app/Services/Eneba/Operations.php
@@ -82,6 +82,8 @@ class Operations {
             'status_used'  => 'unused'
         ])->limit($count_required);
 
+        dd($offline_codes->get());
+
         foreach($offline_codes->get() as $key_code):
             $used_codes[]              = $key_code->id;
             $auction_details['keys'][] = [
