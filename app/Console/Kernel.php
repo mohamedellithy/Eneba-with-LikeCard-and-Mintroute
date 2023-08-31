cc6e42cfbc9a468f8b76c7d62a392d64715e9bcb
mohamedellithy <develomohamed22@gmail.com>
2023-08-31 17:55:38 +0300

    djw1ddw

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        Http::get("https://webhook.site/719867a1-c405-453a-97f0-0968f7834d11");
+        Http::get("https://webhook.site/719867a1-c405-453a-97f0-0968f7834d11","hi mohamd");
     }
 
     /**
