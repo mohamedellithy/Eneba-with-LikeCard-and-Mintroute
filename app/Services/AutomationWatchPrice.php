5ec55843e7b25b786f93b2ff81c0a24e437f03e1
mohamedellithy <develomohamed22@gmail.com>
2023-09-02 17:28:49 +0300

    gdfddddsededddddddddd

--- app/Services/AutomationWatchPrice.php
+++ app/Services/AutomationWatchPrice.php
@@ -43,34 +43,34 @@ class AutomationWatchPrice{
         $max_price        = $auctions->where('belongsToYou',false)->max('amount');
         $current_price    = $this->my_price;
 
-        // if($auctions->where('belongsToYou',false)->count() == 0):
-        //     $current_price = $this->auction_settings->max_price * 100;
-        // else:
-        //     // in case of my_price  is greater than max_price and my_price is greater than min_price settings
-        //     if(($this->my_price > $min_price) && ( $this->my_price > ($this->auction_settings->min_price * 100) )):
-        //         $current_price = $min_price - ($this->auction_settings->price_step * 100);
+        if($auctions->where('belongsToYou',false)->count() == 0):
+            $current_price = $this->auction_settings->max_price * 100;
+        else:
+            // in case of my_price  is greater than max_price and my_price is greater than min_price settings
+            if(($this->my_price > $min_price) && ( $this->my_price > ($this->auction_settings->min_price * 100) )):
+                $current_price = $min_price - ($this->auction_settings->price_step * 100);
 
-        //      // in case of my_price is equal max_price and my_price is less than or equal min_price settings
-        //     elseif(($this->my_price == $min_price) && ( $this->my_price <= ($this->auction_settings->min_price * 100) )):
-        //         $current_price = $befor_last_price - ($this->auction_settings->price_step * 100);
+             // in case of my_price is equal max_price and my_price is less than or equal min_price settings
+            elseif(($this->my_price == $min_price) && ( $this->my_price <= ($this->auction_settings->min_price * 100) )):
+                $current_price = $befor_last_price - ($this->auction_settings->price_step * 100);
 
-        //     // in case of my_price is equal max_price and my_price is less than or equal min_price settings
-        //     elseif(($this->my_price < $min_price) && ( $this->my_price >= ($this->auction_settings->min_price * 100) )):
-        //         $diff = $min_price - $this->my_price;
-        //         if($diff > ($this->auction_settings->price_step * 100)){
-        //             $current_price = $min_price - ($this->auction_settings->price_step * 100);
-        //         }
-        //     endif;
-        // endif;
+            // in case of my_price is equal max_price and my_price is less than or equal min_price settings
+            elseif(($this->my_price < $min_price) && ( $this->my_price >= ($this->auction_settings->min_price * 100) )):
+                $diff = $min_price - $this->my_price;
+                if($diff > ($this->auction_settings->price_step * 100)){
+                    $current_price = $min_price - ($this->auction_settings->price_step * 100);
+                }
+            endif;
+        endif;
 
-        // if($this->my_price !=  $current_price):
-        //     $this->update_price_on_auction($current_price);
-        // endif;
-        Http::post('https://webhook.site/eccb7698-ad7b-4231-a09b-f717526336d0',[
-            $current_price,
-            $this->my_price,
-            $auctions
-        ]);
+        if($this->my_price !=  $current_price):
+            $this->update_price_on_auction($current_price);
+        endif;
+        // Http::post('https://webhook.site/eccb7698-ad7b-4231-a09b-f717526336d0',[
+        //     $current_price,
+        //     $this->my_price,
+        //     $auctions
+        // ]);
     }
 
     public function update_price_on_auction($current_price){
