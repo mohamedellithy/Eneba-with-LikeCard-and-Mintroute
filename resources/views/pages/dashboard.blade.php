e8ab2fb5c88ad26dfe76001eee9de847c2286c8e
mohamedellithy <develomohamed22@gmail.com>
2023-12-20 13:14:43 +0200

    dwssss

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -125,9 +125,43 @@
                                     <img src="{{ asset('/assets/img/icons/unicons/chart-success.png') }}" alt="chart success" class="rounded" />
                                 </div>
                             </div>
-                            <span class="fw-semibold d-block mb-1">عدد الطلبات لايك كارد</span>
+                            <span class="fw-semibold d-block mb-1">عدد الطلبيات لايك كارد</span>
                             <h3 class="card-title mb-2">
                                 {{ \App\Models\ProviderOrder::where('provider_name','LikeCard')->count()  }}
+                            </h3>
+                            <small class="text-success fw-semibold"><i
+                                    class="bx bx-up-arrow-alt"></i> </small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-12 col-3 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="card-title d-flex align-items-start justify-content-between">
+                                <div class="avatar flex-shrink-0">
+                                    <img src="{{ asset('/assets/img/icons/unicons/chart-success.png') }}" alt="chart success" class="rounded" />
+                                </div>
+                            </div>
+                            <span class="fw-semibold d-block mb-1">عدد المزادات</span>
+                            <h3 class="card-title mb-2">
+                                {{ \App\Models\Auction::count()  }}
+                            </h3>
+                            <small class="text-success fw-semibold"><i
+                                    class="bx bx-up-arrow-alt"></i> </small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-12 col-3 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="card-title d-flex align-items-start justify-content-between">
+                                <div class="avatar flex-shrink-0">
+                                    <img src="{{ asset('/assets/img/icons/unicons/chart-success.png') }}" alt="chart success" class="rounded" />
+                                </div>
+                            </div>
+                            <span class="fw-semibold d-block mb-1">عدد المزادات النشطة</span>
+                            <h3 class="card-title mb-2">
+                                {{ \App\Models\Auction::where('status',1)->count()  }}
                             </h3>
                             <small class="text-success fw-semibold"><i
                                     class="bx bx-up-arrow-alt"></i> </small>
