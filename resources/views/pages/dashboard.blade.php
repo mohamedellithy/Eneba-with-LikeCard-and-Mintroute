23caaaacf871d8b1983d6aaef409fdc5f4127329
mohamedellithy <develomohamed22@gmail.com>
2023-12-20 12:32:27 +0200

    dw

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -41,7 +41,21 @@
                         </div>
                     </div>
                 </div>
-
+                <div class="col-lg-6 col-md-12 col-6 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="card-title d-flex align-items-start justify-content-between">
+                                <div class="avatar flex-shrink-0">
+                                    <img src="{{ asset('/assets/img/icons/unicons/chart-success.png') }}" alt="chart success" class="rounded" />
+                                </div>
+                            </div>
+                            <span class="fw-semibold d-block mb-1">Profit</span>
+                            <h3 class="card-title mb-2">$12,628</h3>
+                            <small class="text-success fw-semibold"><i
+                                    class="bx bx-up-arrow-alt"></i> </small>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
