daa89835e570636282b28e5a7e55892458de7d2d
mohamedellithy <develomohamed22@gmail.com>
2023-12-20 02:14:18 +0200

    dwdسيsdddddddddd

--- resources/views/pages/orders/show.blade.php
+++ resources/views/pages/orders/show.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="container-xxl flex-grow-1 container-p-y">
     <h4 class="fw-bold py-3 mb-4">
-        الطلبات
+        رقم الطلبية #{{ $eneba_order->order_id  }}
     </h4>
     <!-- Basic Bootstrap Table -->
     <div class="row">
