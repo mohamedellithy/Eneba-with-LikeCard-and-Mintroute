39f924cda52b6d161ed11d6f09132705df75e6b3
mohamedellithy <develomohamed22@gmail.com>
2024-05-02 18:56:57 +0300

    ADD ORDER ROUTE

--- app/Services/AutomationWatchPrice.php
+++ app/Services/AutomationWatchPrice.php
@@ -23,11 +23,11 @@ class AutomationWatchPrice{
 
         foreach($auctions as $auction):
             $this->auction_settings  = $auction;
-            $command = $this->schedule->call(function(){
-                 $this->getAuctionPrices();
+            $command = $this->schedule->call(function() use($auction){
+                 $this->getAuctionPrices($auction);
             });
 
-            $command->name("Auction no #".$this->auction_settings->id);
+            $command->name("Auction no #".$auction->id);
 
             $command->timezone("Africa/Cairo");
 
@@ -37,81 +37,81 @@ class AutomationWatchPrice{
         endforeach;
     }
 
-    public function getAuctionPrices(){
-        $auctions         = GetAuctionPrices($this->auction_settings->product_id);
+    public function getAuctionPrices($auction){
+        $auctions         = GetAuctionPrices($auction->product_id);
         $this->my_price   = $auctions->where('belongsToYou',true)->value('amount');
         $min_price        = $auctions->where('belongsToYou',false)->min('amount');
-        $befor_last_price = $auctions->firstWhere('amount','>',$this->auction_settings->min_price * 100);
+        $befor_last_price = $auctions->firstWhere('amount','>',$auction->min_price * 100);
         $befor_last_price = isset($befor_last_price) ? $befor_last_price['amount'] : null;
         $max_price        = $auctions->where('belongsToYou',false)->max('amount');
         $current_price    = $this->my_price;
         $section          = null;
 
         if($auctions->where('belongsToYou',false)->count() == 0):
-            $current_price = $this->auction_settings->max_price * 100;
+            $current_price = $auction->max_price * 100;
         else:
             // in case of my_price  is greater than max_price and my_price is greater than min_price settings
-            if(($this->my_price > $min_price) && ( $min_price > ($this->auction_settings->min_price * 100) )):
-                $current_price = $min_price - ($this->auction_settings->price_step * 100);
+            if(($this->my_price > $min_price) && ( $min_price > ($auction->min_price * 100) )):
+                $current_price = $min_price - ($auction->price_step * 100);
 
-                if($current_price < ($this->auction_settings->min_price * 100)):
-                    $current_price = $this->auction_settings->min_price * 100;
+                if($current_price < ($auction->min_price * 100)):
+                    $current_price = $auction->min_price * 100;
                 endif;
                 //$section = "itme 1";
 
              // in case of my_price is equal max_price and my_price is less than or equal min_price settings
-            elseif(($this->my_price >= $min_price) && ( $min_price <= ($this->auction_settings->min_price * 100) )):
+            elseif(($this->my_price >= $min_price) && ( $min_price <= ($auction->min_price * 100) )):
                 if($befor_last_price):
-                    $current_price = $befor_last_price - ($this->auction_settings->price_step * 100);
+                    $current_price = $befor_last_price - ($auction->price_step * 100);
                     if(($befor_last_price - $min_price) == 1):
-                        $current_price = ($this->auction_settings->min_price * 100);
+                        $current_price = ($auction->min_price * 100);
                     endif;
                 endif;
 
-                if($current_price < ($this->auction_settings->min_price * 100)):
-                    $current_price = $this->auction_settings->min_price * 100;
+                if($current_price < ($auction->min_price * 100)):
+                    $current_price = $auction->min_price * 100;
                 endif;
                 //$section = "itme 2";
 
             // in case of my_price is equal max_price and my_price is less than or equal min_price settings
-            elseif(($this->my_price < $min_price) && ( $this->my_price >= ($this->auction_settings->min_price * 100) )):
+            elseif(($this->my_price < $min_price) && ( $this->my_price >= ($auction->min_price * 100) )):
                 $diff = $min_price - $this->my_price;
-                if($diff > ($this->auction_settings->price_step * 100)):
-                    $current_price = $min_price - ($this->auction_settings->price_step * 100);
+                if($diff > ($auction->price_step * 100)):
+                    $current_price = $min_price - ($auction->price_step * 100);
                     //$section = "itme 3";
                 endif;
 
-                if($current_price < ($this->auction_settings->min_price * 100)):
-                    $current_price = $this->auction_settings->min_price * 100;
+                if($current_price < ($auction->min_price * 100)):
+                    $current_price = $auction->min_price * 100;
                 endif;
             endif;
         endif;
 
         if($this->my_price !=  $current_price):
-            $this->update_price_on_auction($current_price);
+            $this->update_price_on_auction($auction,$current_price);
         endif;
         Http::post('https://webhook-test.com/91db77804eebee9be1ef789221f8802e',[
             $current_price,
             $min_price,
             $befor_last_price,
             $section,
-            $this->auction_settings
+            $auction
         ]);
     }
 
-    public function update_price_on_auction($current_price){
-        $form   = $this->auction_settings->current_price;
+    public function update_price_on_auction($auction,$current_price){
+        $form   = $auction->current_price;
         $current_price = $current_price / 100;
-        $this->auction_settings->update([
+        $auction->update([
             'current_price'  => $current_price
         ]);
 
-        $response = $this->eneba_service->update_create_auction($this->auction_settings);
+        $response = $this->eneba_service->update_create_auction($auction);
 
         if(!isset($response['result']['errors'])):
-            $this->auction_settings->logs_auction_price()->create([
+            $auction->logs_auction_price()->create([
                 'from'           => $form,
-                'to'             => $this->auction_settings->current_price,
+                'to'             => $auction->current_price,
                 'eneba_response' => json_encode($response['result']),
                 'status'         => $response['code']
             ]);
