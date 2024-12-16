07e6e0608c298a7e6e119381845224a64b312198
mohamedellithy <develomohamed22@gmail.com>
2024-12-16 13:23:24 +0200

    fix buyer

--- app/Http/Helper.php
+++ app/Http/Helper.php
@@ -108,7 +108,7 @@ if(!function_exists('eneba_single_product')) {
         $eneba_service      = new Eneba($sandbox = false);
         $product_eneba      = $eneba_service->get_single_product($eneba_id);
         $product_eneba['result']['data']['S_product']['auctions'] = $eneba_service->get_competitions($eneba_id)['result'];
-        // dd($product_eneba);
+        dd($product_eneba);
         // $product_eneba      = Cache::rememberForever('eneba_single_product_'.$eneba_id, function() use($eneba_id,$eneba_service){
         //     return $eneba_service->get_single_product($eneba_id)['result']['data'];
         // });
